ce771bc983904d0afcbc6743e099f9e25a7e8df7
Rozzini <Rozzinimr@gmail.com>
2020-04-28 23:14:43 +0300

    Improved score scroller

--- Asteroid2D/Assets/Scripts/ScoreManeger.cs
+++ Asteroid2D/Assets/Scripts/ScoreManeger.cs
@@ -11,33 +11,19 @@ public class ScoreManeger : MonoBehaviour
 
     private string url = "https://localhost:44333/api/Models";
 
-
     public RectTransform prefab;
+
     public RectTransform content;
+
     private List<Player> playersList = new List<Player>();
+
     void Start()
     {
         StartCoroutine(GetRequest(url));
     }
 
-    void Update()
-    {
 
-    }
-
-    public void GetText()
-    {
-        int x = 0;
-        int y = 0;
-        for(int i = 0; i < playersList.Count; i++)
-        {
-            //CreateText(playersList[i]);
-            y -= 20;
-            x -= 1;
-        }
-    }
-
-    public void CreateText(/*Transform canvas_transform, float x, float y, Player player, int font_size, int iterator*/)
+    public void CreateText()
     {
 
         foreach (Transform child in content)
@@ -49,7 +35,7 @@ public class ScoreManeger : MonoBehaviour
         {
             var instance = GameObject.Instantiate(prefab.gameObject) as GameObject;
             instance.transform.SetParent(content, false);
-            string textToPrint = p.PlayerName + "    " + p.Score;
+            string textToPrint = "  " + p.PlayerName + "    " + p.Score;
             Text text = instance.GetComponent<Text>();
 
             text.text = textToPrint;
@@ -57,30 +43,7 @@ public class ScoreManeger : MonoBehaviour
             text.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
             text.color = Color.white;
         }
-        //string textToPrint = player.PlayerName + "    " + player.Score;
-        //Instantiate(TextBox, gameObject.transform.position, Quaternion.identity);
-        //Text text = TextBox.GetComponent<Text>();
-        //text.text = textToPrint;
-        //text.fontSize = 15;
-        //text.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
-        //text.color = Color.white;
-
-        //string BoxName = "Score" + iterator + 1;
-        //string textToPrint = player.PlayerName + "    " + player.Score;
-
-        //GameObject UItextGO = new GameObject(BoxName);
-        //UItextGO.transform.SetParent(canvas_transform);
-
-        //RectTransform trans = UItextGO.AddComponent<RectTransform>();
-        //trans.anchoredPosition = new Vector2(x, y);
-
-        //Text text = UItextGO.AddComponent<Text>();
-        //text.text = textToPrint;
-        //text.fontSize = font_size;
-        //text.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
-        //text.color = Color.white;
-
-        //return UItextGO;
+       
     }
 
     IEnumerator GetRequest(string url)
