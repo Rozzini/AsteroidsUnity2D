1987530e9048b7ad533f46b572797b5f82633b8e
Rozzini <Rozzinimr@gmail.com>
2020-04-30 13:38:42 +0300

    js script Added

---
+++ AsteroidsWebServer/Asteroid2DWeb/wwwroot/js/AsteroidsMovement.js
@@ -0,0 +1,29 @@
+﻿var x;
+var changeDirection;
+
+function setup() {
+	createCanvas(1920, 1080);
+	x = 1;
+	changeDirection = false;
+}
+
+function draw() {
+	background(220);
+	ellipse(x, 50, 50);
+	if (x > width) {
+		changeDirection = true
+	}
+
+	else if (x <= 0) {
+		changeDirection = false
+	}
+
+	if (x >= 0 && changeDirection == false) {
+		x = x + 1
+	}
+
+	else if (changeDirection == true) {
+		x = x - 1
+	}
+
+}
